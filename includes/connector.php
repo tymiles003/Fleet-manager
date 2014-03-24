962650ce9e27b031a374583be502d85d01a8bece
steveroyall <stephen.royall@gmail.com>
2014-03-24 22:20:41 +0000

    local environment setup

---
+++ includes/connector.php
@@ -0,0 +1,26 @@
+<?php
+$mysql_user = 'fleet';
+$mysql_password = 'pass';
+$db_name = 'fleet';
+$debug = true;
+
+$link = mysql_connect('localhost:8889', $mysql_user, $mysql_password);
+if (!$link) {
+	if($debug){
+    die('Could not connect: ' . mysql_error());
+	}
+}
+
+$db_selected = mysql_select_db($db_name, $link);
+if (!$db_selected) {
+	if($debug){
+    die ('Can\'t use' . $db_name  . mysql_error());
+}
+}else{
+	if($debug){
+	echo 'Connected to ' . $db_name . ' DB successfully';
+	}
+}
+
+mysql_close($link);
+?>
--- index.php
+++ index.php
@@ -1,1 +1,29 @@
-index.php
+<!doctype html>
+<?php 
+include "includes/definitions.php";
+include "includes/connector.php";
+?>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+
+
+</head>
+<body>
+	<nav>
+		<ul>
+			<li>HOME</li>
+			<li>SETTINGS</li>
+			<li>MY FLEET</li>
+			<li>HELP</li>
+		</ul>
+	</nav>
+	<section class="main">
+		
+	</section>
+	<h1><?php echo SITE_NAME ?></h1>
+	
+</body>
+</html>
+
